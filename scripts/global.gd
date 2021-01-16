89eaf882c508abd6bb7e5c5c5f132e9bc416c2da
sktbang <igordsmolina@gmail.com>
2021-01-16 12:17:59 -0300

    Reconstrução do projeto com novo GIT
    O projeto precisou ser reconstruido com outra configuração de GIT pois a anterior quebrou, sendo assim o código não poderia ser atualizado.

---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var custom_variables = {} 
+
+func _ready():
+	pass # Replace with function body.
+
---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+const GRAVITY = 50
+const JUMP_POWER = 1000
+const SPEED = 600
+const UP = Vector2(0, -1)
+var motion = Vector2() #função que recebe índices x e y como parâmetro. Usado 
+#para objetos 2D
+	
+func _physics_process(delta): #processamento contínuo da física do objeto 
+	#nesse caso jogador) com o delta(leitura de fps) como parâmetro
+	if Input.is_action_pressed("ui_cancel"):
+	  get_tree().quit()
+	
+	motion.y += GRAVITY #gravidade
+	
+	if Input.is_action_pressed("ui_right"): #enquanto pressionar para a direita
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("run")
+		
+	elif Input.is_action_pressed("ui_left"): #enquanto pressionar para a esquerda
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("run")
+	else:
+		motion.x = 0 
+		$Sprite.play("idle")
+		
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -JUMP_POWER
+	else:
+		$Sprite.play("jump")
+	
+	motion = move_and_slide(motion, UP)
